b673419e9f361696ae250d5c83d58fdd4a11fddf
noob-slayer <rajpriye.siddharth@gmail.com>
2026-08-19 15:02:22 +0530

    Remove the "buy me a coffee" hyperlink
    Pointed at the template's original author's donation page (buymeacoffee.com/sidcodes),
    not this deployment's owner -- kept as plain text credit, dropped the now-misleading
    link-affordance styling (underline, hover color) along with the link itself.

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,13 +1,8 @@
-// TODO: swap in the real Buy Me a Coffee username once you have one
-const COFFEE_URL = "https://www.buymeacoffee.com/sidcodes";
-
 export function Credit() {
   return (
     <div className="credit">
       <span>vibe coded by Sid with &lt;3</span>
-      <a className="credit__coffee" href={COFFEE_URL} target="_blank" rel="noreferrer">
-        buy me a coffee
-      </a>
+      <span>buy me a coffee</span>
     </div>
   );
 }
